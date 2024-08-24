2c19e8468529602a638dc7ab810b2537aa72dcfa
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-24 20:23:25 +0900

    first commit

---
+++ elevate/crm/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def homepage(request):
+
+
+    return render(request, 'crm/index.html')
+
+
+
+
+
+def register(request):
+    return render(request, 'crm/register.html')
+
